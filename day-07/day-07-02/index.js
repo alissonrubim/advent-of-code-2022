0f5da925d54e48029f1c417ad27a1588a53a0c16
Alisson Rubim <alissonrubim@gmail.com>
2022-12-07 13:20:39 +0100

    Add day 07 solution

---
+++ day-07/day-07-02/index.js
@@ -0,0 +1,90 @@
+const fs = require('fs')
+const path = require('path')
+
+const main = () => {
+  const rawInput = fs.readFileSync(path.join(__dirname, './inputs/inputs-02.txt'), 'utf-8');
+
+  const cmdOutput = rawInput.split("\n");
+
+  const cmdBlock = [];
+  cmdOutput.forEach((line) => {
+    const [a,b,c] = line.split(" ");
+    if(a === "$" && b === "cd"){
+      cmdBlock.push([]);
+    }
+    cmdBlock[cmdBlock.length-1].push(line)
+  })
+
+  let currentPath = [];
+  const folders = [];
+  cmdBlock.forEach((block) => {
+    block.forEach((blockCmd) => {
+      const [a,b,c] = blockCmd.split(" ");
+      if(a === "$" && b === "cd"){ //If is a cd command
+        if(c === "/"){
+          currentPath = ["/"]; // Return to root
+        }else if(c === ".."){
+          currentPath.splice(currentPath.length-1, 1); // Remove level
+        }else{
+          currentPath.push(`${c}/`); // Enter level
+        }
+      }
+
+      else if(a === "$" && b === "ls"){ //If is a ls command
+        folders.push({
+          path: currentPath.join(""),
+          size: 0,
+          children: []
+        })
+      }
+
+      else { //If is a file or dir info
+        if(a === "dir"){ //If is dir
+          const folder = folders.find((x) => x.path === currentPath.join(""));
+          folder.children.push({
+            tyee: "D",
+            name: b,
+            size: 0
+          })
+        }
+        else { //If is file
+          const folder = folders.find((x) => x.path === currentPath.join(""));
+          folder.children.push({
+            type: "F",
+            name: b,
+            size: parseInt(a, 10)
+          })
+        }
+      }
+    })
+  })
+
+  const getFolderSize = (folder) => {
+    let totalFolderSize = 0;
+    folder.children.forEach((child) => {
+      if(child.type === "F"){
+        totalFolderSize += child.size;
+      }else{``
+        const childFolder = folders.find((x) => x.path === (folder.path + `${child.name}/`));
+        child.size = getFolderSize(childFolder);
+        childFolder.size = child.size;
+        totalFolderSize += child.size; 
+      }
+    })
+    folder.size = totalFolderSize;
+    return folder.size;
+  }
+
+  const usedSpace = getFolderSize(folders[0]); //Calculate all the folders size
+  const diskSpace = 70000000;
+  const updateSpace = 30000000;
+  const unsetSpace = diskSpace - usedSpace;
+  const spaceNeeded = updateSpace - unsetSpace;
+
+  // Filter folders that have at least the size that we need
+  // Then map to only numbers
+  // Sort asc
+  // Get the first
+  console.info(folders.filter((x) => x.size >= spaceNeeded).map((x) => x.size).sort((a, b) => a-b)[0]);
+}
+main();
